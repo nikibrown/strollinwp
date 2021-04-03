f37bab836b5f9cf18c7d836df7a2033774c1cc07
Niki Brown <nikibrown@Niki-Browns-MacBook-Pro.local>
2021-04-02 18:00:54 -0700

    sponsors page

---
+++ page-sponsors.php
@@ -0,0 +1,52 @@
+<?php get_header("nothome"); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    <main>
+		<section class="section--dark-blue">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<h2><?php the_title(); ?></h2>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="section--white">		
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<h2>Featured Sponsors</h2>
+						<ul class="sponsors-list">
+							<?php if( have_rows('featured_sponsors') ): while( have_rows('featured_sponsors') ): the_row(); ?>
+								<?php $featured_sponsor = get_sub_field('featured_sponsor'); ?>
+								<li><?php echo $featured_sponsor; ?></li>
+							<?php endwhile; endif; ?>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>		
+
+		<section class="section--white">		
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<h2>Curretn and Past Sponsors</h2>
+						<ul class="sponsors-list">
+							<?php if( have_rows('sponsors') ): while( have_rows('sponsors') ): the_row(); ?>
+								<?php $sponsor = get_sub_field('sponsor'); ?>
+								<li><?php echo $sponsor; ?></li>
+							<?php endwhile; endif; ?>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>		
+
+    </main>
+
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>     
