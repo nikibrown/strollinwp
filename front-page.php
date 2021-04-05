8b341dbddacd2f2811c62d89e8028b07349584cf
Niki Brown <nikibrown@Niki-Browns-MacBook-Pro.local>
2021-04-05 11:03:57 -0700

    front page custom fields

--- front-page.php
+++ front-page.php
@@ -3,8 +3,7 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
     <main>
-		<?php if( have_rows('homepage_hero') ): ?>
-    		<?php while( have_rows('homepage_hero') ): the_row(); 
+		<?php if( have_rows('homepage_hero') ): while( have_rows('homepage_hero') ): the_row(); 
 
 				// Get sub field values.
 				$homepage_hero_image = get_sub_field('homepage_hero_image');
@@ -12,66 +11,74 @@
 				$homepage_hero_left_headline = get_sub_field('homepage_hero_left_headline'); 
 				$homepage_hero_left_text = get_sub_field('homepage_hero_left_text'); 
 				$homepage_hero_left_link_text = get_sub_field('homepage_hero_left_link_text'); 
+
+				$homepage_hero_right_image = get_sub_field('homepage_hero_right_image');
+				$homepage_hero_right_content = get_sub_field('homepage_hero_right_content');
+				$cta_link = get_sub_field('homepage_hero_right_cta_link'); 
+				$cta_link_url = $cta_link['url'];
+				$cta_link_text = $cta_link['title'];
+				$cta_link_target = $cta_link['target'] ? $cta_link['target'] : '_self';
+
 			?>
-        <section class="section--photo-bleed-right">
-			<div class="left-content">
-				<img src="<?php bloginfo("template_url"); ?>/assets/img/strollin-logo-color.svg" alt="Strollin for the Colon" class="m-4" />
-			</div>
-			<div class="right-content" style="background-image: url(<?php echo esc_url( $homepage_hero_image ); ?>)"></div>
-		</section>
-
-        <section class="section--half-half-extend">
-			<div class="container">
-				<div class="row align-items-center section--light">
-					<div class="col-lg-6">
-						<img src="<?php echo esc_url( $homepage_hero_left_image); ?>" class="float-left" alt="<?php echo $homepage_hero_left_headline; ?>" />
-						<h3><?php echo $homepage_hero_left_headline; ?></h3>
-						<p>
-							<?php echo $homepage_hero_left_text; ?> <a href="<?php the_sub_field('homepage_hero_left_link'); ?>"><?php echo $homepage_hero_left_link_text;?></a>
-						</p>
-					</div>
-
-					<!-- TODO: where does this content come from? Manual entry on homepage? -->
-					<div class="col-lg-6 d-none d-lg-block section--dark">
-						<img src="http://via.placeholder.com/270x60?text=Toni's+Race" alt="" />
-						<p class="lead">
-							Optam debit fuga. MeFicabo. Dus porume inisitas fuga. Et quis expernam re
-							voluptae sint as essint iligenis reiur sed quam, is perum quiassiti nos
-							invelitio.
-						</p>
-
-						<p>MONTH XX, 2021</p>
-					</div>
+			<section class="section--photo-bleed-right">
+				<div class="left-content">
+					<img src="<?php bloginfo("template_url"); ?>/assets/img/strollin-logo-color.svg" alt="Strollin for the Colon" class="m-4" />
 				</div>
-			</div>
-		  
+				<div class="right-content" style="background-image: url(<?php echo esc_url( $homepage_hero_image ); ?>)"></div>
+			</section>
 
-			<div class="mobile-content d-lg-none d-xl-none section--dark">
+			<section class="section--half-half-extend">
 				<div class="container">
-					<div class="row">
-						<div class="col-12">
-							<img src="http://via.placeholder.com/270x60?text=Toni's+Race" alt="" />
-							<p class="lead">
-								Optam debit fuga. MeFicabo. Dus porume inisitas fuga. Et quis expernam re
-								voluptae sint as essint iligenis reiur sed quam, is perum quiassiti nos
-								invelitio.
+					<div class="row align-items-center section--light">
+						<div class="col-lg-6">
+							<img src="<?php echo esc_url( $homepage_hero_left_image); ?>" class="float-left" alt="<?php echo $homepage_hero_left_headline; ?>" />
+							<h3><?php echo $homepage_hero_left_headline; ?></h3>
+							<p>
+								<?php echo $homepage_hero_left_text; ?> <a href="<?php the_sub_field('homepage_hero_left_link'); ?>"><?php echo $homepage_hero_left_link_text;?></a>
 							</p>
+						</div>
 
-							<p>MONTH XX, 2021</p>
+						<div class="col-lg-6 d-none d-lg-block section--dark">
+							<img src="<?php echo $homepage_hero_right_image; ?>" alt="Toni's Race" />
+							<div class="lead">
+								<?php echo $homepage_hero_right_content; ?>
+							</div>
+
+							<?php if( $cta_link ): ?>
+								<a href="<?php echo esc_url( $cta_link_url ); ?>" target="<?php echo esc_attr( $cta_link_target ); ?>" class="btn btn-dark">
+									<?php echo esc_html( $cta_link_text ); ?>
+								</a>
+							<?php endif; ?>	
 						</div>
 					</div>
 				</div>
-			</div>
-		</section>
+			
 
-		  <?php endwhile; ?>
-			<?php endif; ?>
+				<div class="mobile-content d-lg-none d-xl-none section--dark">
+					<div class="container">
+						<div class="row">
+							<div class="col-12">
+								<img src="<?php echo $homepage_hero_right_image; ?>" alt="Toni's Race" />
+								<div class="lead">
+									<?php echo $homepage_hero_right_content; ?>
+								</div>
+
+								<?php if( $cta_link ): ?>
+									<a href="<?php echo esc_url( $cta_link_url ); ?>" target="<?php echo esc_attr( $cta_link_target ); ?>" class="btn btn-dark">
+										<?php echo esc_html( $cta_link_text ); ?>
+									</a>
+								<?php endif; ?>	
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<?php endwhile; endif; ?>
 
 
-		<?php if( have_rows('homepage_blue_section') ): ?>
-    	<?php while( have_rows('homepage_blue_section') ): the_row(); 
+		<?php if( have_rows('homepage_blue_section') ): while( have_rows('homepage_blue_section') ): the_row(); 
 
-			// Get sub field values.
 			$headline_1 = get_sub_field('headline_1');
 			$headline_2 = get_sub_field('headline_2');
 			$headline_3 = get_sub_field('headline_3');
@@ -98,28 +105,24 @@
 					</div>
 				</div>
 			</section>
-		<?php endwhile; ?>
-		<?php endif; ?>
+		<?php endwhile; endif; ?>
 
-		<?php if( have_rows('homepage_photo_banner_section') ): ?>
-		<?php while( have_rows('homepage_photo_banner_section') ): the_row(); ?>
+		<?php if( have_rows('homepage_photo_banner_section') ): while( have_rows('homepage_photo_banner_section') ): the_row(); ?>
 		<?php $homepage_photo_banner_image = get_sub_field('homepage_photo_banner_image'); ?>
 			
 			<section class="section--photo-banner-large" style="background-image: url(<?php echo esc_url( $homepage_photo_banner_image ); ?>)"></section>
 
-		<?php endwhile; ?>
-		<?php endif; ?>
+		<?php endwhile; endif; ?>
 
-		<?php if( have_rows('homepage_white_section') ): ?>
-		<?php while( have_rows('homepage_white_section') ): the_row(); ?>
-		<?php 
-			$photo_1 = get_sub_field('photo_1');
-			$photo_2 = get_sub_field('photo_2');
-			$headline_1 = get_sub_field('headline_1');
-			$headline_2 = get_sub_field('headline_2');
-			$content_1 = get_sub_field('content_1');
-			$content_2 = get_sub_field('content_2');
-		?>
+		<?php if( have_rows('homepage_white_section') ): while( have_rows('homepage_white_section') ): the_row(); ?>
+			<?php 
+				$photo_1 = get_sub_field('photo_1');
+				$photo_2 = get_sub_field('photo_2');
+				$headline_1 = get_sub_field('headline_1');
+				$headline_2 = get_sub_field('headline_2');
+				$content_1 = get_sub_field('content_1');
+				$content_2 = get_sub_field('content_2');
+			?>
 
 			<section class="section--half-half section--light">
 				<div class="container">
@@ -139,8 +142,7 @@
 					</div>
 				</div>
 			</section>
-		<?php endwhile; ?>
-		<?php endif; ?>
+		<?php endwhile; endif; ?>
 
     </main>
 
